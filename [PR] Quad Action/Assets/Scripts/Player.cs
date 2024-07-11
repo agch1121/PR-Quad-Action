b3105a03dec2a617a3b07bdaf8f00bf4b35d7d15
agch1121 <155516846+agch1121@users.noreply.github.com>
2024-07-12 00:08:32 +0900

    Add Jump&Dodge, Add Items
    점프, 회피 동작 추가
    다양한 아이템들 디자인 및 추가

--- [PR] Quad Action/Assets/Scripts/Player.cs
+++ [PR] Quad Action/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -8,30 +9,91 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
+
+    bool isJump;
+    bool isDodge;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
+
+    Rigidbody rigid;
     Animator anim;
     void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
 
     
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Dodge();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal"); // Axis 값을 정수로 반환하는 함수
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk"); // 버튼을 누를때만 작동
-
+        jDown = Input.GetButtonDown("Jump");
+    }
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        if(isDodge)
+        {
+            moveVec = dodgeVec;
+        }
+     
 
-        
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
-
+    }
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
 
+    void Jump()
+    {
+        if(jDown && moveVec == Vector3.zero  && !isJump && !isDodge)
+        {
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse); //Impulse : 즉발적인 힘
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.5f);
+        }
+    }
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }
